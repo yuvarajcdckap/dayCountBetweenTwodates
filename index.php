6dbb6a82414cfd06e09367e03204d8d3ccfa6bd0
yuvarajcdckap <yuvarajcdckap@gmail.com>
2023-05-22 22:31:35 +0530

    main

--- index.php
+++ index.php
@@ -5,19 +5,10 @@ $endDate=readline('end date:');
 // echo strtotime($startDate);
 function dayCountCalculation($startDate,$endDate){
     $diff = strtotime($endDate) - strtotime($startDate);
-
     return abs(round($diff / 86400));
-
-    
 }
 
  $daycount=dayCountCalculation($startDate,$endDate);
-
-    if($daycount<=1){
-        echo $daycount,"-day";
-    }
-    else{
-        echo $daycount,"-days";
-    }
+ echo ($daycount<=1) ? $daycount.":day":  $daycount.":days";
 
 ?>
